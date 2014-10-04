432a5ab73eda9a2602d33edb4c048d33e58e0770
Scott Slack <kd7vbs@gmail.com>
2014-10-04 15:01:39 -0700

    itance Calculation

--- API/API/Services/Distance.cs
+++ API/API/Services/Distance.cs
@@ -6,8 +6,24 @@ using System.Web;
 namespace API.Services
 {
     public static class Distance {
+        // Theta = longitude
+        // Phi = latitude
+        private const double EarthRadius = 6371000.0d; // wikipedia
         public static Double Meters(Double lat1, Double lon1, Double lat2, Double lon2) {
-            return 2.9d;
+            double theta = lon1;
+            double phi = lat1;
+            
+            double x1 = EarthRadius * Math.Cos(lon1) * Math.Sin(lat1);
+            double y1 = EarthRadius * Math.Sin(lon1) * Math.Sin(lat1);
+            double z1 = EarthRadius * Math.Cos(lat1);
+
+            double x2 = EarthRadius * Math.Cos(lon2) * Math.Sin(lat2);
+            double y2 = EarthRadius * Math.Sin(lon2) * Math.Sin(lat2);
+            double z2 = EarthRadius * Math.Cos(lat2);
+
+            double distance = Math.Sqrt( (x1 - x2)*(x1 - x2) + (y1-y2)*(y1 - y2) + (z1-z2)*(z1-z2));
+
+            return distance;
         }
     }
 }
