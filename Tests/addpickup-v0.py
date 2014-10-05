d7f64de5c6da96afff924508a6e64a9eb0ae952d
slinger360 <dilsha21@gmail.com>
2014-10-04 20:06:24 -0700

    Changed addpickup-v0.py to use appropriate URL.

--- Tests/addpickup-v0.py
+++ Tests/addpickup-v0.py
@@ -1,3 +1,3 @@
 import requests
-post_pickup = {'Id':'1634', 'FirstName':'Dirty', 'LastName':'Dan', 'Address':'1634 Junkyard Lane', 'City':'MochasareYummy', 'Zip':'98101', 'Phone':'4256218392', 'Email':'clevar@bored.com', 'Items':'human livers', 'Notes':'in my basement, door code is 666', 'Schedule':'00:00-03:00'}
-post_response = requests.post(url='../api/pickups', data=post_pickup)
+post_pickup = {'Notes':'in my basement, door code is 666','Items':'human livers','Donor':{'OptIn':True,'FirstName':'Dirty','DeviceId':'666','Phone':'4256218392','Email':'clevar@bored.com','LastName':'Dan'},'Zip':'98101','City':'MochasareYummy','Address':'1634 Junkyard Lane'}
+post_response = requests.post(url='../api/donors', data=post_pickup)
