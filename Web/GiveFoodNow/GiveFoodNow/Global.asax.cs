17fff664c16e465d7b7cf76ecfc4ddf57d0af258
Jack Pines <github@jackpines.info>
2014-10-04 23:07:38 -0700

    Created MVC5 Web & WebAPI 2 project

---
+++ Web/GiveFoodNow/GiveFoodNow/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace GiveFoodNow
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<API.Models.FoodCoalitionAppContext>());
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Web/GiveFoodNow/GiveFoodNow/Models/Member.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API.Models {
+    public class Member 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ContactFirstName { get; set; }
+
+        [Required]
+        public string ContactLastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+        
+        [Required]
+        public long RangeInMeters { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public Double Latitude { get; set; }
+
+        public Double Longitude { get; set; }
+    }
+}
