b3a4e468b37b3c4898577cff39c473692b809cdb
slinger360 <dilsha21@gmail.com>
2014-10-04 18:00:31 -0700

    Fixed minor typo in getpickups-v0.py test script.

--- Tests/getpickups-v0.py
+++ Tests/getpickups-v0.py
@@ -2,4 +2,4 @@ import requests
 lat=47.597685
 longi=-122.327957
 distance=32186.9
-requests.get(url='../api/pickups/range/%s/$s/%s' % lat,longi,distance)
+requests.get(url='http://70.42.168.133/api/pickups/range/%s/%s/%s' % lat,longi,distance)
