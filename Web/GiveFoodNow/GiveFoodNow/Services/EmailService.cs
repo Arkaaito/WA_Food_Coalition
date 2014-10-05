5941f44fb96425a76dde4d6e10e45dd38382a2d0
Jack Pines <github@jackpines.info>
2014-10-05 10:12:52 -0700

    Added content to About page
    Also let ReSharper clean up includes.

--- Web/GiveFoodNow/GiveFoodNow/Services/EmailService.cs
+++ Web/GiveFoodNow/GiveFoodNow/Services/EmailService.cs
@@ -1,11 +1,8 @@
 ﻿using API.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Configuration;
 
 namespace API.Services {
